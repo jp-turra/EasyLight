f2ddfdae7c659a637f4724fa557b0b6bf3e1e4f3
jp-turra <jp-turra@users.noreply.github.com>
2021-02-24 00:03:31 -0300

    verificação de requisitos

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,6 +5,24 @@
 </template>
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  created () {
+    window.plugins.speechRecognition.isRecognitionAvailable(
+      (success) => {
+        console.log('Recognition Available')
+        window.plugins.speechRecognition.hasPermission(
+          (success) => { console.log('Has permission'); this.$store.commit('setRecognitionStatus', true) },
+          () => {
+            console.log("Hasn't permission")
+            window.plugins.speechRecognition.requestPermission(
+              (success) => { console.log('Recognition Authorized'); this.$store.commit('setRecognitionStatus', true) },
+              () => { console.log('Recognition Unauthorized'); this.$store.commit('setRecognitionStatus', false) }
+            )
+          }
+        )
+      },
+      () => { console.log('Recognition Unavailable'); this.$store.commit('setRecognitionStatus', false) }
+    )
+  }
 }
 </script>
