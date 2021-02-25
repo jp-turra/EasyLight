5ca10cc0e507b5c8b5074dcedc27b65c0a8eb4cd
jp-turra <jp-turra@users.noreply.github.com>
2021-02-24 23:42:51 -0300

    Arduino

--- arduino/Easy_Light/Easy_Light.ino
+++ arduino/Easy_Light/Easy_Light.ino
@@ -1,15 +1,21 @@
 #include <SoftwareSerial.h>
 #define RX 2
 #define TX 4
-#define LAMP 8
+#define LAMP1 8
+#define LAMP2 9
+#define LAMP3 10
 
 boolean state = false;
 String incoming = "";
 SoftwareSerial bluetooth(RX, TX); // RX, TX
 
 void setup() {
-  pinMode(LAMP, OUTPUT);
-  digitalWrite(LAMP, LOW);
+  pinMode(LAMP1, OUTPUT);
+  pinMode(LAMP2, OUTPUT);
+  pinMode(LAMP3, OUTPUT);
+  digitalWrite(LAMP1, LOW);
+  digitalWrite(LAMP2, LOW);
+  digitalWrite(LAMP3, LOW);
   delay(300);
   Serial.begin(9600);
   bluetooth.begin(9600);
@@ -38,7 +44,9 @@ void executar (String action){
     acionar();
   }
 }
-
+void definirLampada (){
+  
+}
 void acionar (){
   if (state==true){
     desligar();
@@ -49,9 +57,9 @@ void acionar (){
 
 void ligar(){
   state = true;
-  digitalWrite(LAMP, HIGH);
+  digitalWrite(LAMP1, HIGH);
 }
 void desligar(){
   state = false;
-  digitalWrite(LAMP, LOW);
+  digitalWrite(LAMP1, LOW);
 }
